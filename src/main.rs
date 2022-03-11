779a8d8f01aa830a7e364b9ce5baf3aceebebc09
molnattila991 <molnarattila991@gmail.com>
2022-03-11 08:21:32 +0100

    (add) 404 html (edit) return index else 404

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,34 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let contents = fs::read_to_string("index.html").unwrap();
+    if buffer.starts_with(get) {
+        let contents = fs::read_to_string("index.html").unwrap();
 
-    let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-        contents.len(),
-        contents
-    );
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
 
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    } else {
+        let contents = fs::read_to_string("404.html").unwrap();
+
+        let response = format!(
+            "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+            contents.len(),
+            contents
+        );
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
 
 // HTTP-Version Status-Code Reason-Phrase CRLF
