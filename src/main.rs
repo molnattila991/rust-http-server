23ac7659af06f42a234762984b4d2c4f6f726470
molnattila991 <molnarattila991@gmail.com>
2022-03-11 08:31:49 +0100

    (edit) one threaded, with sleep 5 sec

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io::prelude::*;
 use std::io::Read;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:8989").unwrap();
@@ -19,13 +21,16 @@ fn handle_connection(mut stream: TcpStream) {
     //println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status_line, filename) = 
-        if buffer.starts_with(get) {
-            ("HTTP/1.1 200 OK", "index.html")
-        } else {
-            ("HTTP/1.1 404 Not FOUND", "404.html")
-        };
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 Not FOUND", "404.html")
+    };
 
     let contents = fs::read_to_string(filename).unwrap();
 
